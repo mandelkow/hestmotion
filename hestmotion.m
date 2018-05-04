af32afa1ce838283264eb779b989e5ade6bdae7c
HM <Hendrik.Mandelkow@nih.gov>
2018-05-04 16:08:46 -0400

    ++ Wrapper hestmotion 3BR

--- hestmotion.m
+++ hestmotion.m
@@ -1,39 +1,55 @@
 function [M,P,Img] = hestmotion(Img,varargin)
-% hEstMotion **1b: Motion correction using estMotionMulti3() by <david.heeger@nyu.edu>.
+% hEstMotion:**3BR++: Motion correction for 2D/3D image/volumes (affine trafo)...
+%                  using estMotionMulti3() by <david.heeger@nyu.edu>.
 %
-% SYNTAX: [M,P,Img] = hestmotion(Img,[RefImg,]MultiScale,numIiters,Minitial,rotFlag,robustFlag,CB,SC)
+% SYNTAX: [M,P,Img] = hestmotion(Img,[RefImg,]numIiters,Minitial,rotFlag,robustFlag,CB,SC)
 %
-% MultiScale = ? use multiscale algorithm
-% numIters =
-% Minitial =
-% rotFlag = RIGID BODY only
+% numIters = # of iterations default= 1. If vector e.g. [2 3 4],...
+%            use *multiscale* algorithm with 4 iterations at 1/4 size, 3
+%            iter. at 1/2 size and 2 iter. at full size.
+% Minitial = starting point for optimisation (passed between iterations)
+% rotFlag = RIGID BODY motion only (no shearing and scaling)
 % M(:,:,n) = Affine trafo matrix for img n
-% P = Motion correction parameters
-% Img = Corrected imgs
+% P(n,:) = 12 Motion correction parameters (computed from M(:,:,n))
+%          3x translation (XYZ), 3x rotation, 3x scaling, 3x shearing
+% Img = Corrected images
+%
+% NOTE: Large difference in dynamic range between images compared are
+%       disadvantageous. Normalisation may improve results. Also, try
+%       multiscale (initial downsampling) for large images.
+%
 % SEE: estMotion2, estMotion3
 
+% AUTH: HM, 08.2008, 3B: Replace param. MultiScale with numIters= [1 2 3].
+%                        Display P(0,:), in case of only 1 image.
 % AUTH: HM, 04.2006, 2B: Implement DEFAULT.
 % AUTH: HM, 04.2006, 1B1: BUGFIX: 2D RefImg for 2D correction.
 % AUTH: HM, 02.2005, 1B.
 
 DISP = 1;
-% [RefImg,MultiScale,numIters,Minitial,rotFlag,robustFlag,CB,SC]
-DEFAULT = [{Img(:,:,:,1)},{0},{1},{[]},{1},{0},{[]},{[]}];
+% [RefImg,numIters,Minitial,rotFlag,robustFlag,CB,SC]
+DEFAULT = [{Img(:,:,:,1)},{1},{[]},{1},{0},{[]},{[]}];
 if isempty(varargin) || length(varargin{1}) < 2, % No RefImg given.
     varargin = [{[]},varargin];
     ImgNo = 2;
-else,
+else
     ImgNo = 1;
 end
 tmp = find(~cellfun('isempty',varargin));
-DEFAULT(tmp) = varargin(tmp);
+DEFAULT(tmp) = varargin(tmp); % Replace defaults by input.
 varargin = DEFAULT;
 clear DEFAULT
-[RefImg,MultiScale,numIters,Minitial,rotFlag,robustFlag,CB,SC] = ...
-    deal(varargin{:});
-varargin(1:2) = [];
-disp(cell2struct([{MultiScale},varargin],{'MultiScale','Iterations',...
-    'Minitial','RigidFlag','RobustFlag','CB','SC'},2));
+[RefImg,numIters,Minitial,rotFlag,robustFlag,CB,SC] = deal(varargin{:});
+varargin(1) = []; % Remove RefImg.
+
+if length(numIters) < 2,
+    disp(cell2struct(varargin,...
+        {'Iterations','Minitial','RigidFlag','RobustFlag','CB','SC'},2));
+else
+    disp(cell2struct(varargin,...
+        {'Iterations_MultiScale','Minitial','RigidFlag','RobustFlag','CB','SC'},2));
+end
+% Remove trailing empty inputs from varargin.
 while ~isempty(varargin) && isempty(varargin{end}),
     varargin(end) = [];
 end;
@@ -48,17 +64,17 @@ if length(ImgSz) < 4,   % 2D registration
     MSG = strrep(MSG,'3D','2D');
     M = zeros([3,3,ImgSz(end)]);
     Flag2D = 1;
-else,
+else
     Flag2D = 0;
     M = zeros([4,4,ImgSz(end)]);
 end;
-if isempty(MultiScale) || ~MultiScale, % No Multi Scale approach
+if length(numIters) < 2, % No Multi Scale approach
     FUN = strrep(FUN,'Multi','Iter');
-end;
-if numIters == 1,   % Only one iteration.
-    FUN = strrep(FUN,'Iter','');
-    varargin(1:2) = []; % Delete numIters and Minitial
-end;
+    if numIters == 1,   % Only one iteration.
+        FUN = strrep(FUN,'Iter','');
+        varargin(1:2) = []; % Delete numIters and Minitial
+    end;
+end
 FUN = str2func(FUN);
 
 if isempty(rotFlag) || rotFlag,
@@ -72,10 +88,10 @@ wstate = warning('off','hctranspose:complex');
 for ImgNo=ImgNo:ImgSz(end),
     if Flag2D,
         M(:,:,ImgNo) = feval(FUN,RefImg,Img(:,:,ImgNo),varargin{:});
-    else,
+    else
         M(:,:,ImgNo) = feval(FUN,RefImg,Img(:,:,:,ImgNo),varargin{:});
     end;
-    if 0, %numIters > 1,
+    if numIters > 1, % *** HM! 2012-07
         varargin{2} = M(:,:,ImgNo); % = Minitial
     end;
     fprintf('\b\b\b\b\b%5u',ImgNo);
@@ -92,6 +108,12 @@ fprintf(' \t DONE.\n');
 warning(wstate);
 
 %% Disp motion params:
+if DISP || (nargout > 1),
+    P = zeros(size(M,3),12);
+    for ImgNo=1:size(M,3),
+        P(ImgNo,:) = hspm_imatrix(M(:,:,ImgNo));
+    end;
+end
 if DISP,
     figure('name','hestmotion');
     
@@ -108,30 +130,32 @@ if DISP,
 %         legend('X','Y','Z','location','best','orientation','horizontal');
 %     end;
     
-    for ImgNo=1:size(M,3),
-        P(ImgNo,:) = hspm_imatrix(M(:,:,ImgNo));
-    end;
+    P = cat(1,[0 0 0 0 0 0 1 1 1 0 0 0],P); % Display 0 in case size(M,3)==1.
+    tmp = [0:size(P,1)-1]';
+    
     subplot(3,1,1)
-    plot(P(:,1:3));
+    plot(tmp,P(:,1:3));
     ylabel('translation / voxel'); xlabel('image volumes');
     legend('X','Y','Z','location','best','orientation','horizontal');
     
     subplot(3,1,2)
-    plot(180/pi*P(:,4:6));
+    plot(tmp,180/pi*P(:,4:6));
     ylabel('rotation / deg'); %xlabel('image volumes');
     %legend('X','Y','Z','location','best','orientation','horizontal');
     
     subplot(6,1,5)
-    plot(P(:,7:9));
+    plot(tmp,P(:,7:9));
     ylabel('scale factor'); %xlabel('image volumes');
     %legend('X','Y','Z','location','best','orientation','horizontal');
     
     subplot(6,1,6)
-    plot(P(:,10:12));
+    plot(tmp,P(:,10:12));
     ylabel('shear'); %xlabel('image volumes');
     %legend('X','Y','Z','location','best','orientation','horizontal');
     
     set(findobj(gcf,'type','axes'),'FontSize',6,'FontWeight','normal');
+    
+    P(1,:) = []; % Remove extra.
 end;
 
 %%
@@ -142,7 +166,7 @@ if nargout > 2,
             Img(:,:,ImgNo) = warpAffine2(Img(:,:,ImgNo),M(:,:,ImgNo));
             fprintf('\b\b\b\b\b%5u',ImgNo);
         end;
-    else,
+    else
         fprintf('\nTransform %u volumes... %5u',size(M(:,:,:),3),0);
         for ImgNo=1:ImgSz(end),
             Img(:,:,:,ImgNo) = warpAffine3(Img(:,:,:,ImgNo),M(:,:,ImgNo));
